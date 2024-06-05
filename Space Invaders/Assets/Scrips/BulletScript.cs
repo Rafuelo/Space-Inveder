289a3f53b1bb07e0e6e2c985610b9b3024dc362e
Rafuelo <122719183+Rafuelo@users.noreply.github.com>
2024-06-05 10:51:10 +0300

    More hot fixes
    Fixed an issue when players bullets despawn immediately
    Fixed animation error of baricades

--- Space Invaders/Assets/Scrips/BulletScript.cs
+++ Space Invaders/Assets/Scrips/BulletScript.cs
@@ -4,25 +4,37 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
+    public LogicScript logic;
     public float deadZone = 10;
     public float bulletSpeed = 5;
     public bool isPlayer = true;
 
+    private void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (isPlayer)
-            MoveBullet(Vector3.up);
+        if (logic.GetIsAllMoving())
+        {
+            if (isPlayer)
+                MoveBullet(Vector3.up);
 
+            else
+                MoveBullet(Vector3.down);
+        }
+        
         else
-            MoveBullet(Vector3.down);
+            MoveBullet(Vector3.zero);
     }
 
     private void MoveBullet(Vector3 direction)
     {
         transform.position += direction * Time.deltaTime * bulletSpeed;
 
-        if (Mathf.Abs(transform.position.x) >= deadZone)
+        if (Mathf.Abs(transform.position.y) >= deadZone)
             Destroy(gameObject);
     }
 
