214a427527248518f9ad8c192ed0cc560a71fe29
Rafuelo <122719183+Rafuelo@users.noreply.github.com>
2024-06-09 12:24:25 +0300

    Added more durability to baricades

--- Space Invaders/Assets/Scrips/BaricadeScript.cs
+++ Space Invaders/Assets/Scrips/BaricadeScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaricadeScript : MonoBehaviour
+public class HalfBaricadeScript : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public Sprite half;
@@ -10,18 +10,18 @@ public class BaricadeScript : MonoBehaviour
     public BoxCollider2D collider2d;
     public float offsetX;
 
-    public int durability = 9;
+    public int durability = 21;
 
     void Update()
     {
-        if (durability == 10)
+        if (durability == 14)
         {
             collider2d.offset = new Vector2(collider2d.offset.x, -0.25f);
             collider2d.size = new Vector2(collider2d.size.x, 0.5f);
             spriteRenderer.sprite = half;
         }
 
-        if (durability == 5)
+        if (durability == 7)
         {
             collider2d.offset = new Vector2(offsetX, -0.37f);
             collider2d.size = new Vector2(0.5f, 0.26f);
